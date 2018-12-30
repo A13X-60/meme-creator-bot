27ebdea936db260afd98117d9e85e725baf7fa0b
Michael Fridrikhson <fridrihson.1701@gmail.com>
2018-12-30 21:56:00 +0200

    Meme name fix

--- Memebase.py
+++ Memebase.py
@@ -74,7 +74,7 @@ offering_sword = Meme({(426, 254): (121, 28), (497, 148): (256, 299), (458, 290)
                       (255, 255, 255))
 domino_effect = Meme({(392, 108): (141, 435), (315, 171): (0, 83), (314, 125): (316, 239)}, 'impact.ttf',
                      (255, 255, 255))
-price_charles_steals_wheel = Meme({(398, 225): (0, 128), (326, 236): (150, 393), (408, 279): (368, 234)}, 'impcat.ttf',
+price_charles_steals_wheel = Meme({(398, 225): (0, 128), (326, 236): (150, 393), (408, 279): (368, 234)}, 'impact.ttf',
                                   (255, 255, 255))
 Memes = {'drake': drake, 'scroll of truth': scroll_of_truth, 'expanding brain': expanding_brain,
          'who would win': who_would_win, 'the rock driving': the_rock_driving, 'sleeping shaq': sleeping_shaq,
@@ -93,4 +93,4 @@ Memes = {'drake': drake, 'scroll of truth': scroll_of_truth, 'expanding brain': 
          'tom shoots himself': tom_shoots_himself, 'goodbye high school': goodbye_high_school,
          'kissing pair': kissing_pair, 'useless gun': useless_gun, 'connor pushes circle': connor_pushes_circle,
          'lisa simpson presentation': lisa_simpson_presentation, 'offering sword': offering_sword,
-         'domino effect': domino_effect}
+         'domino effect': domino_effect, 'prince charles steals wheel': price_charles_steals_wheel}
